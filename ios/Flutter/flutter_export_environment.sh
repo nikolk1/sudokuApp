9c4be06cae91701eeb34a478dc05d2d079b6ca65
nikolk1 <60259833+nikolk1@users.noreply.github.com>
2020-06-12 12:07:15 +0300

    init commit

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\user\Desktop\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\user\Desktop\soduku_temp"
+export "FLUTTER_APPLICATION_PATH=C:\Users\user\Desktop\soduku_app"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
