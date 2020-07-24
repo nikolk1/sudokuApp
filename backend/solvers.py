46461671b73a07ed0e2857943193854d2cb6ba7b
nikolk1 <nikolk202@gmail.com>
2020-07-24 23:45:41 +0300

    created class for future solving algorithms

---
+++ backend/solvers.py
@@ -0,0 +1,10 @@
+from backend.genBoard import Board
+
+class SudokuSolversMethod:
+    @staticmethod
+    def backtracking(b: Board):
+        pass
+
+    @staticmethod
+    def stochastic_search(b: Board):
+        pass
