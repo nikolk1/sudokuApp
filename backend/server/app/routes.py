988ee7b1ffe1cec34dd239acf79a370824d50de9
nikolk1 <nikolk202@gmail.com>
2020-09-19 15:42:13 +0300

    Connect board generation to server

--- backend/server/app/routes.py
+++ backend/server/app/routes.py
@@ -1,12 +1,18 @@
 from app import app
 from flask import jsonify
-
+from backend.genBoard import GeneratePuzzle, GenerateBoardMethods, Difficulty
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Welcome to my Sudoku Generator !!"
 
-@app.route('/generate/<level>')
-def generate(level):
-    return jsonify(level=level)
+@app.route('/generate/<difficulty>')
+def generate(difficulty: str):
+    board = GenerateBoardMethods.backtracking_method()
+    if not board:
+        return "500 Internal Error"
+    else:
+        puzzle = GeneratePuzzle(board, Difficulty[difficulty])
+        puzzle.generate()
+        return puzzle.board.__str__()
